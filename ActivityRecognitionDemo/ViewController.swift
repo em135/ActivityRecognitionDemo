e3039d16fdc2406cac7749bda6a0bfe66581d261
Emil Nielsen <emilqz@gmail.com>
2019-11-14 12:06:12 +0100

    Initial Commit

---
+++ ActivityRecognitionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ActivityRecognitionDemo
+//
+//  Created by Emil Nielsen on 14/11/2019.
+//  Copyright © 2019 Emil Nielsen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
